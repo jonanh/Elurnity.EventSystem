b30210e058c5d31713b92d59135a4ee9f181821c
Jon Ander Hernandez <jonan.h@gmail.com>
2016-04-02 19:01:56 +0300

    Add the event trigger window and the event debugger

---
+++ Editor/EventDebuggerWindow.cs
@@ -0,0 +1,82 @@
+﻿using UnityEditor;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Vexe.Runtime.Types;
+
+namespace Events
+{
+    public class EventDebuggerWindow : VexeEditorWindow
+    {
+        public List<Event> events = new List<Event>();
+        private List<Event> capturedEvents = new List<Event>();
+
+        [Show]
+        public void Clear()
+        {
+            events.Clear();
+            capturedEvents.Clear();
+            Repaint();
+        }
+
+        [ShowType(typeof(Event))]
+        public Type eventType = typeof(Event);
+
+        [Show]
+        public void Filter()
+        {
+            events.Clear();
+            events.AddRange(
+                from capturedEvent in capturedEvents
+                where filter(capturedEvent)
+                select capturedEvent);
+            Repaint();
+        }
+
+        protected bool filter(Event evt)
+        {
+            return eventType.IsAssignableFrom(evt.GetType());
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            EventListener.globalListener += OnEvent;
+            EditorApplication.playmodeStateChanged += OnPlayModeChanged;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            EventListener.globalListener -= OnEvent;
+            EditorApplication.playmodeStateChanged += OnPlayModeChanged;
+        }
+
+        protected void OnEvent(Event evt)
+        {
+            if (filter(evt))
+            {
+                events.Add(evt);
+            }
+            capturedEvents.Add(evt);
+            Repaint();
+        }
+
+        protected void OnPlayModeChanged()
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                Clear();
+            }
+        }
+
+        public static class MenuItems
+        {
+            [MenuItem("Window/Events/Event debugger")]
+            public static void ShowWindow()
+            {
+                EditorWindow.GetWindow<EventDebuggerWindow>("Event Debugger");
+            }
+        }
+    }
+}
---
+++ Editor/EventTriggerWindow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using System;
+using System.Collections.Generic;
+using Vexe.Runtime.Types;
+
+namespace Events
+{
+    public class EventTriggerWindow : VexeEditorWindow
+    {
+        [Show, Inline]
+        public Event @event;
+
+        [Show]
+        public void Trigger()
+        {
+            foreach (var element in Selection.transforms)
+            {
+                element.Trigger(@event);
+            }
+        }
+
+        public static class MenuItems
+        {
+            [MenuItem("Window/Events/Trigger Events")]
+            public static void ShowMyWindow()
+            {
+                EditorWindow.GetWindow<EventTriggerWindow>("Event Trigger");
+            }
+        }
+    }
+}
